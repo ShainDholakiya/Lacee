2c789c4623d0417e4b66d588f083883b1985b033
Shain Dholakiya <shainshain99@gmail.com>
2019-05-03 23:40:47 -0400

    profile css

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -4,6 +4,15 @@ import { Link } from "react-router-dom";
 const ProfileActions = () => {
   return (
     <div className="mb-4">
+      <p className="laceedup-info">
+        <strong>Fill out your profile and click</strong>
+        <Link to="/bored-or-needhelp" className="btn btn-dark btn-laceedup">
+          <i className="fas fa-angle-double-right text-info mr-1" />
+          Get Laceed Up
+        </Link>
+        <strong>to start your lacee experience</strong>
+      </p>
+      <br />
       <Link to="/edit-profile" className="btn btn-light btn-action">
         <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
       </Link>
@@ -14,11 +23,6 @@ const ProfileActions = () => {
       <Link to="/add-education" className="btn btn-light btn-action">
         <i className="fas fa-graduation-cap text-info mr-1" />
         Add Education
-      </Link>
-      <br />
-      <Link to="/bored-or-needhelp" className="btn btn-dark btn-laceedup">
-        <i className="fas fa-angle-double-right text-info mr-1" />
-        Get Laceed Up
       </Link>
     </div>
   );
