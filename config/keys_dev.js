7b48ce6fb1af489fa48f6ad5e3ab20873f2d2944
Shain Dholakiya <shainshain99@gmail.com>
2020-07-28 09:07:54 -0400

    updates

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://shainshain99:123@lace-biofa.mongodb.net/test?retryWrites=true",
+    "mongodb+srv://shainshain99:Hello12345@lace-biofa.mongodb.net/test?retryWrites=true",
   secretOrKey: "secret"
 };
